da6b5c87a638d3cb472cd2a17a73a9e095321dcf
Moses Lee <moses1998lee@gmail.com>
2025-08-13 20:44:25 +0800

    chore(run_scheduling): mounted scheduling_bp to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,37 @@
-
 from flask import Flask
-from flask_pymongo import PyMongo
 from flask_cors import CORS
+from flask_pymongo import PyMongo
+
 from app.config import Config
 
 # Initialize MongoDB
 mongo = PyMongo()
 
+
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    CORS(app, origins=[
-        'http://localhost:5173',  # Keep for local testing
-        'https://ui-genesis.vercel.app',
-        'https://ui-genesis-dev.vercel.app'
-    ])
+    CORS(
+        app,
+        origins=[
+            "http://localhost:5173",  # Keep for local testing
+            "https://ui-genesis.vercel.app",
+            "https://ui-genesis-dev.vercel.app",
+        ],
+    )
 
     # Initialize extensions
     mongo.init_app(app)
-    
+
     # Register blueprints
-    from app.routes.main import main_bp
     from app.routes.auth import auth_bp
-    from app.routes.excel import excel_bp
+    from app.routes.configs import overall_config_bp
     from app.routes.employee import employee_bp
     from app.routes.employee_column_mapping import employee_column_mapping_bp
+    from app.routes.excel import excel_bp
+    from app.routes.main import main_bp
+    from app.routes.run_scheduling import scheduling_bp
     from app.routes.schedule_config import schedule_config_bp
-    from app.routes.configs import overall_config_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
@@ -35,5 +40,6 @@ def create_app(config_class=Config):
     app.register_blueprint(employee_column_mapping_bp)
     app.register_blueprint(schedule_config_bp)
     app.register_blueprint(overall_config_bp)
-    
+    app.register_blueprint(scheduling_bp)
+
     return app
